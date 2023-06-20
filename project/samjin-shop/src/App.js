b01e73e61971e96fd90a870f6cecc62f701d2621
kim bo seong <njunju77@nate.com>
2023-06-20 14:44:51 +0900

    samjin-shop 부트스트랩+라우터

---
+++ project/samjin-shop/src/App.js
@@ -0,0 +1,91 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import data from "./pages/productData";
+import About from "./pages/About";
+import Detail from "./pages/Detail";
+
+import { Container, Nav, Navbar, Row, Col } from "react-bootstrap";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+function App() {
+  const navigate = useNavigate();
+  const [bests] = useState(data);
+  return (
+    <div className="App">
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Samjin-shop
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              HOME
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/about");
+              }}
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/about/info");
+              }}
+            >
+              Information
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/about/laca");
+              }}
+            >
+              Location
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Container>
+              <img src={process.env.PUBLIC_URL + "./images/visual_main_01.jpg"} alt="" />
+              <Row>
+                {bests.map((best, index) => {
+                  return (
+                    <Col key={index}>
+                      <Link to={`detail/${index}`}>
+                        <img src={best.image} alt="product_img" style={{ width: 280 }}></img>
+                        <h4>{best.title}</h4>
+                        <p>{best.desc}</p>
+                        <p>{best.price}</p>
+                      </Link>
+                    </Col>
+                  );
+                })}
+                <Col> 1 of 1 </Col>
+              </Row>
+            </Container>
+          }
+        />
+        <Route path="/" element={<div>Home Page</div>}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="info" element={<div>Information</div>}></Route>
+        <Route path="loca" element={<div>Location</div>}></Route>
+        <Route path="detail/:id" element={<Detail bests={bests} />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ project/samjin-shop/src/pages/Detail.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function Detail(props) {
+  const { bests } = props;
+  const { id } = useParams();
+  return (
+    <div>
+      <h2>Detail Page</h2>
+      <img src={bests[id].image} alt="" style={{ width: 500 }} />
+      <h4>{bests[id].title}</h4>
+      <p>{bests[id].price}</p>
+    </div>
+  );
+}
